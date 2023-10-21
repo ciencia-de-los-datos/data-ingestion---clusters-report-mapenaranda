872430a4360feed8f0962940b2114a19e116e61c
mapenaranda <125393614+mapenaranda@users.noreply.github.com>
2023-10-21 19:40:59 +0200

    Add files via upload

--- pregunta.py
+++ pregunta.py
@@ -14,8 +14,15 @@ import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_fwf('clusters_report.txt', index_col = False, header = None, skiprows = 4 ,
+                 names = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave','principales_palabras_clave'])
+    columns_fill = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave']
+    df[columns_fill] = df[columns_fill].fillna(method='ffill')
+    df=df.groupby(columns_fill)['principales_palabras_clave'].agg(list).reset_index()
+    df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].astype(int)
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].replace({' %':''}, regex=True).replace({',':'.'}, regex=True).astype(float)
+    df['cluster'] = df['cluster'].astype(int)
+    df['principales_palabras_clave']=df['principales_palabras_clave'].apply(lambda x: ' '.join(x))
+    df['principales_palabras_clave'] = [item.replace('.', '').replace('    ', ' ').replace('   ', ' ').replace('  ', ' ') for item in df['principales_palabras_clave']]
 
     return df
