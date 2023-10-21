f033ded54f2b251a129f098ba31105a5f4b19407
mapenarandam <mapenarandam@unal.edu.co>
2023-10-21 20:23:26 +0200

    intento4

--- pregunta.py
+++ pregunta.py
@@ -13,9 +13,11 @@ import pandas as pd
 
 
 def ingest_data():
-    ######
+
     df = pd.read_fwf('clusters_report.txt', index_col = False, header = None, skiprows = 4 ,
                  names = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave','principales_palabras_clave'])
+    
+    
     columns_fill = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave']
     df[columns_fill] = df[columns_fill].fillna(method='ffill')
     df=df.groupby(columns_fill)['principales_palabras_clave'].agg(list).reset_index()
